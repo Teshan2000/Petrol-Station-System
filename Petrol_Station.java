732a215d640d835650c2a91b6a83d7fc19a64e63
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-04-11 21:29:09 +0530

    Switch cases
    Switch cases were added to the system for user inputs

--- Petrol_Station.java
+++ Petrol_Station.java
@@ -1,3 +1,48 @@
+import java.util.Scanner;
+
 public class Petrol_Station {
+    public static void main(String[] args) {
+        
+        System.out.println("Welcome to our Petrol Station System!");
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Choose your urgency");
+        System.out.println("Enter the number of your choice: ");  
+        int urgency = reader.nextInt();
+
+        switch (urgency) {
+            case 1:
+            Urgent();
+            break; 
+
+            case 2:
+            Normal(); 
+            break;
+
+            case 3:
+            NotUrgent(); 
+            break;
+        }
+
+        System.out.println("Choose your distance");
+        System.out.println("Enter the number of your choice: ");  
+        int distance = reader.nextInt();
+
+        switch (distance) {
+            case 1:
+                Faraway();                
+            break;
+
+            case 2:
+                NotSoFar(); 
+            break;
+
+            case 3:
+                Near();
+            break;
+}
+
+        System.out.println("");
+    }
     
 }
