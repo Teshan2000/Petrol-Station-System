83c3a98b5dc981f0a89d34be183dea33edd77b8e
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-04-16 01:07:38 +0530

    For Loop
    A for loop was created to get inputs from 9 users

--- Petrol_Station.java
+++ Petrol_Station.java
@@ -6,86 +6,90 @@ public class Petrol_Station {
         System.out.println("Welcome to our Petrol Station System!");
         Scanner reader = new Scanner(System.in);
 
-        System.out.println("Choose your urgency");
-        System.out.println("\t 1. Urgent"); 
-        System.out.println("\t 2. Normal"); 
-        System.out.println("\t 3. Not Urgent"); 
+        for(int i = 1; i <= 9; i++) {
 
-        System.out.println("Enter the number of your choice: ");  
-        int urgency = reader.nextInt();
+            System.out.println("Choose your urgency");
+            System.out.println("\t 1. Urgent"); 
+            System.out.println("\t 2. Normal"); 
+            System.out.println("\t 3. Not Urgent"); 
 
-        switch (urgency) {
-            case 1:
-            Urgent();
-            break; 
+            System.out.println("Enter the number of your choice: ");  
+            int urgency = reader.nextInt();
 
-            case 2:
-            Normal(); 
-            break;
+            switch (urgency) {
+                case 1:
+                Urgent();
+                break; 
 
-            case 3:
-            NotUrgent(); 
-            break;
+                case 2:
+                Normal(); 
+                break;
 
-            default:
-            System.out.println("Enter again");
+                case 3:
+                NotUrgent(); 
+                break;
+
+                default:
+                System.out.println("Enter again");
+            }
+
+            System.out.println("Choose your distance");
+            System.out.println("\t 1. Far Away"); 
+            System.out.println("\t 2. Not So Far"); 
+            System.out.println("\t 3. Near"); 
+
+            System.out.println("Enter the number of your choice: ");  
+            int distance = reader.nextInt();
+
+            switch (distance) {
+                case 1:
+                    Faraway();                
+                break;
+
+                case 2:
+                    NotSoFar(); 
+                break;
+
+                case 3:
+                    Near();
+                break;
+
+                default:
+                System.out.println("Enter again");
+            }
+
+
+            if(urgency == 1 && distance == 1){
+                System.out.println("Your Token is 1");
+            }
+            else if(urgency == 1 && distance == 2){
+                System.out.println("Your Token is 2");
+            }
+            else if(urgency == 1 && distance == 3){
+                System.out.println("Your Token is 3");
+            }
+            else if(urgency == 2 && distance == 1){
+                System.out.println("Your Token is 4");
+            }
+            else if(urgency == 2 && distance == 2){
+                System.out.println("Your Token is 5");
+            }
+            else if(urgency == 2 && distance == 3){
+                System.out.println("Your Token is 6");
+            }
+            else if(urgency == 3 && distance == 1){
+                System.out.println("Your Token is 7");
+            }
+            else if(urgency == 3 && distance == 2){
+                System.out.println("Your Token is 8");
+            }
+            else if(urgency == 3 && distance == 3){
+                System.out.println("Your Token is 9");
+            }
         }
 
-        System.out.println("Choose your distance");
-        System.out.println("\t 1. Far Away"); 
-        System.out.println("\t 2. Not So Far"); 
-        System.out.println("\t 3. Near"); 
-
-        System.out.println("Enter the number of your choice: ");  
-        int distance = reader.nextInt();
-
-        switch (distance) {
-            case 1:
-                Faraway();                
-            break;
-
-            case 2:
-                NotSoFar(); 
-            break;
-
-            case 3:
-                Near();
-            break;
-
-            default:
-            System.out.println("Enter again");
-        }
-
-
-        if(urgency == 1 && distance == 1){
-            System.out.println("Token is 1");
-        }
-        else if(urgency == 1 && distance == 2){
-            System.out.println("Token is 2");
-        }
-        else if(urgency == 1 && distance == 3){
-            System.out.println("Token is 3");
-        }
-
-        else if(urgency == 2 && distance == 1){
-            System.out.println("Token is 4");
-        }
-        else if(urgency == 2 && distance == 2){
-            System.out.println("Token is 5");
-        }
-        else if(urgency == 2 && distance == 3){
-            System.out.println("Token is 6");
-        }
-
-        else if(urgency == 3 && distance == 1){
-            System.out.println("Token is 7");
-        }
-        else if(urgency == 3 && distance == 2){
-            System.out.println("Token is 8");
-        }
-        else if(urgency == 3 && distance == 3){
-            System.out.println("Token is 9");
-        }
+        System.out.println("------------------------------------"); 
+        System.out.println("------ This is your Token List------"); 
 
     }
         
